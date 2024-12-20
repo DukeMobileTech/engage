e8303a7fec5f79e5cdbe35c129f784834a696ff4
Leonard Ng'eno <dranoel.bravo@gmail.com>
2024-12-20 13:13:46 -0500

    migration issues

--- db/migrate/20241218215155_rename_session_to_sitting.rb
+++ db/migrate/20241218215155_rename_session_to_sitting.rb
@@ -2,9 +2,9 @@ class RenameSessionToSitting < ActiveRecord::Migration[8.0]
   def change
     rename_table :sessions, :sittings
     rename_column :attendances, :session_id, :sitting_id
-    rename_index :attendances, 'index_attendances_on_session_id', 'index_attendances_on_sitting_id'
-    rename_index :attendances, 'index_attendances_on_session_id_and_participant_id', 'index_attendances_on_sitting_id_and_participant_id'
+    rename_index :attendances, 'index_attendances_on_session_id', 'index_attendances_on_sitting_id' if index_exists?(:attendances, :session_id)
+    rename_index :attendances, 'index_attendances_on_session_id_and_participant_id', 'index_attendances_on_sitting_id_and_participant_id' if index_exists?(:attendances, [ :session_id, :participant_id ])
     rename_column :responses, :session_id, :sitting_id
-    rename_index :responses, 'index_responses_on_session_id', 'index_responses_on_sitting_id'
+    rename_index :responses, 'index_responses_on_session_id', 'index_responses_on_sitting_id' if index_exists?(:responses, :session_id)
   end
 end
