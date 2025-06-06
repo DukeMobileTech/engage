f20cf1bcac3ba0ddf69f8f931f5b65d092ac4724
Leonard Ng'eno <dranoel.bravo@gmail.com>
2025-06-06 11:13:39 -0400

    fix participant sorting and ordering error

--- app/controllers/site_participants_controller.rb
+++ app/controllers/site_participants_controller.rb
@@ -2,7 +2,10 @@ class SiteParticipantsController < ApplicationController
   # GET /site_participants or /site_participants.json
   def index
     @site = Site.find(params.expect(:site_id))
-    @query = @site.site_participants.ransack(params[:query])
+    @query = @site.site_participants.joins(:participant).ransack(params[:query])
     @site_participants = @query.result(distinct: true)
+                               .select("site_participants.*, participants.name AS participant_name, participants.study_id AS participant_study_id, participants.category AS participant_category")
+                               .includes(:participant)
+                               .order("participants.name ASC")
   end
 end
