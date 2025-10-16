e0706eb7e4993b8ac177dcba930025f73447b253
Leonard Ng'eno <dranoel.bravo@gmail.com>
2025-10-16 07:50:07 -0400

    change settings/curriculum names

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,5 @@
 STATES = [ [], [ "NC", "NC" ], [ "SC", "SC" ] ]
 URBANICITIES = [ [], [ "Urban", "Urban" ], [ "Suburban", "Suburban" ], [ "Rural", "Rural" ] ]
-SETTINGS = [ [], [ "In School", "In School" ], [ "Community Based", "Community Based" ], [ "Juvenile Justice", "Juvenile Justice" ], [ "Foster Care", "Foster Care" ], [ "Clinic Based", "Clinic Based" ], [ "Online", "Online" ] ]
+SETTINGS = [ [], [ "In-school", "In-school" ], [ "Other out-of-school time/community", "Other out-of-school time/community" ], [ "Juvenile justice", "Juvenile justice" ], [ "Youth in foster care", "Youth in foster care" ], [ "Clinic-based", "Clinic-based" ], [ "Technology-based", "Technology-based" ], [ "Faith-based", "Faith-based"], ["Youth experiencing homelessness", "Youth experiencing homelessness"] ]
 PARTICIPANT_TYPES = [ [], [ "Caregiver", "Caregiver" ], [ "Youth", "Youth" ],  [ "Youth Serving Professional", "Youth Serving Professional" ] ]
 GRANT_NUMBER = "TP1AH000301"
