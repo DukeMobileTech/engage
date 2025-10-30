6eea7edebd5f0607b8cdb45b3b5a95912465d336
Leonard Ng'eno <dranoel.bravo@gmail.com>
2025-10-30 11:38:00 -0400

    add a fk on delete cascade

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_29_191342) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_30_152402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -295,7 +295,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_29_191342) do
   add_foreign_key "responses", "questionnaires"
   add_foreign_key "responses", "sittings"
   add_foreign_key "section_participant_responses", "responses"
-  add_foreign_key "section_participant_responses", "section_participants"
+  add_foreign_key "section_participant_responses", "section_participants", on_delete: :cascade
   add_foreign_key "section_participants", "participants"
   add_foreign_key "section_participants", "sections"
   add_foreign_key "sections", "curriculums"
