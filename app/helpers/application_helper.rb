1e0942f63a1227d8697f553dd9c1ead2d375d5ab
Leonard Ng'eno <dranoel.bravo@gmail.com>
2026-04-01 09:46:17 -0400

    redesign nav-bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def is_nav_active?(*controllers)
+    active = controllers.any? { |controller| params[:controller] == controller }
+    active ? "active" : ""
+  end
 end
